7f7bd54a7ebb0c1e96cba777cfcdd52ecda6a6d7
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2021-06-12 19:40:46 +0500

    added check updates

---
+++ functions/checkUpdates.js
@@ -0,0 +1,19 @@
+module.exports = async () => {
+    if (!require('node-fetch')) return;
+    const packageData = await require('node-fetch')(`https://registry.npmjs.com/ultrax`).then(text => text.json())
+    if (require('../package.json').version !== packageData['dist-tags'].latest) {
+        console.log('\n\n')
+        console.log('\x1b[32m' + '---------------------------------------------------')
+        console.log('\x1b[32m' + '| @ ultrax                        - [] X |')
+        console.log('\x1b[32m' + '---------------------------------------------------')
+        console.log('\x1b[33m' + `|            The module is\x1b[31m out of date!\x1b[33m           |`)
+        console.log('\x1b[35m' + '|             New version is available!           |')
+        console.log('\x1b[34m' + `|                  ${require('../package.json').version} --> ${packageData['dist-tags'].latest}                |`)
+        console.log('\x1b[36m' + '|             Run "npm i ultrax@latest"           |')
+        console.log('\x1b[36m' + '|                    to update!                   |')
+        console.log('\x1b[37m' + `|          View the full changelog here:          |`)
+        console.log('\x1b[31m' + '|       https://www.npmjs.com/package/ultrax      |')
+        console.log('\x1b[32m' + '---------------------------------------------------\x1b[37m')
+        console.log('\n\n')
+    }
+};
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const sleep = require('./functions/sleep'),
-passGen = require('./functions/passGen'),
-bin = require('./functions/bin'),
-inviteLogger = require("./functions/invite-logger")
+    passGen = require('./functions/passGen'),
+    bin = require('./functions/bin'),
+    inviteLogger = require("./functions/invite-logger"),
+    _checkUpdate = require("./functions/checkUpdates")
 
+
+_checkUpdate()
 module.exports = {
     sleep,
     passGen,
