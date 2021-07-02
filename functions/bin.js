ee6e25c268f7f8a833f469633b59e88a96fb10e5
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2021-07-02 07:49:04 -0700

    fix of bin :)

--- functions/bin.js
+++ functions/bin.js
@@ -4,14 +4,12 @@
  * @returns {Promise<String>}
  */
 module.exports = async (code, edit = true) => {
-            let editable = 1;
-            if (edit === false) editable = 0;
     // replaces all '`' with air.
     code = code.replace("```js", "").replace("```", "").replace("`", "");
     // initilizes params from nodejs 'url' npm
     const params = (new(require("url").URLSearchParams)());
     params.append('content', code);
-    params.append('edible', editable);
+    if (edit === false) params.append('edible', 1);
     let a = await require("node-fetch")("https://pocket-inc.ml/bin/api/create.php", {
         body: params,
         method: 'post'
