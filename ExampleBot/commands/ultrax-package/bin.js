1d8bab70e94e5a646c6756fd245c95369b3ed09f
BIntelligent <65684589+BIntelligent@users.noreply.github.com>
2021-07-02 07:38:25 -0700

    Added a new Arg in bin to make editable bin or no

--- ExampleBot/commands/ultrax-package/bin.js
+++ ExampleBot/commands/ultrax-package/bin.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js")
 exports.run = async (client, message, args, logger) => {
     try {
-        
         const ultrax = require('ultrax')
         if (!args.join(' ')) return message.channel.send('What do you wnat to bin?');
         else {
-            const bin = await ultrax.bin(args.join(' '));
+            const bin = await ultrax.bin(args.join(' '), true); // true means editable while false means not editable
             message.channel.send('Here i binned the code ' + bin)
         }
 
--- functions/bin.js
+++ functions/bin.js
@@ -3,12 +3,15 @@
  * @param {String} code Code to be bin
  * @returns {Promise<String>}
  */
-module.exports = async (code) => {
+module.exports = async (code, edit = true) => {
+            let editable = 1;
+            if (edit === false) editable = 0;
     // replaces all '`' with air.
     code = code.replace("```js", "").replace("```", "").replace("`", "");
     // initilizes params from nodejs 'url' npm
     const params = (new(require("url").URLSearchParams)());
     params.append('content', code);
+    params.append('edible', editable);
     let a = await require("node-fetch")("https://pocket-inc.ml/bin/api/create.php", {
         body: params,
         method: 'post'
