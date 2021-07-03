bebf8fa21edfeb6eca88cc558b7f6dc013d15828
Linker <66379820+Linker-123@users.noreply.github.com>
2021-07-03 20:22:03 +0400

    Fix a really bad thing

--- functions/sleep.js
+++ functions/sleep.js
@@ -3,18 +3,9 @@
  * @param {Number} milliseconds Sleep time (ms)
  * @returns {Promise<void>}
  */
-function sleep(milliseconds) {
-    if (!milliseconds) throw new TypeError("Time isn't specified")
-    const date = Date.now();
-    let currentDate = null;
-    do {
-        currentDate = Date.now();
-    } while (currentDate - date < milliseconds);
-}
+const sleep = (ms) => {
+   if (!ms) throw new TypeError("Time isn't specified");
+   return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 module.exports = sleep;
-
-/**
- * const wait = require('util').promisify(setTimeout)
- * wait(5000)
- */
