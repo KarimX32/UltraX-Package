09f602ad0070572c8635844336dcf183324655f0
SuperSweatyTejas <77419101+SuperSweatyTejas@users.noreply.github.com>
2021-07-08 08:28:54 -0400

    Update daBaby.js

--- functions/daBaby.js
+++ functions/daBaby.js
@@ -7,14 +7,14 @@ const Canvas = require('canvas')
  */
 
 module.exports = async function daBaby(url) {
-    const canvas = Canvas.createCanvas(900, 900);
+    const canvas = Canvas.createCanvas(867, 892);    
     const ctx = canvas.getContext('2d');
     const background = await Canvas.loadImage('https://media.pitchfork.com/photos/5c7d4c1b4101df3df85c41e5/1:1/w_320/Dababy_BabyOnBaby.jpg');
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     
     const imageURL = await Canvas.loadImage(url);
 
-    ctx.drawImage(imageURL, 285, 260, 300, 300);
+    ctx.drawImage(imageURL, 285, 260, 280, 300);
 
     return new Discord.MessageAttachment(canvas.toBuffer(), 'DaBaby.png')
 
