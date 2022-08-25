bd8451a1b3a742835e0e4bbbb2c30a77f1ffd205
HyperNaili <gamernaili.101010@gmail.com>
2022-08-25 21:18:51 +0200

    feat: eslint fixes and name changes
    Commit message says it all.

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const db = require("mongoose");
+const db = require('mongoose');
 const sleep = require('./functions/sleep'),
-    passGen = require('./functions/passGen'),
-    bin = require('./functions/bin'),
-    inviteLogger = require("./functions/invite-logger"),
-    _checkUpdate = require("./functions/checkUpdates"),
-    ButtonPaginator = require("./functions/button-embed-reactor"),
-    welcomeImage = require('./functions/welcomeImage'),
-    Wikipedia = require('./classes/wikipedia'),
-    sussyBaka = require('./classes/sussybaka'),
-    remind = require('./functions/remind'),
-    logger = require('./functions/logger'),
-    daBaby = require('./functions/daBaby'),
-    boostImage = require('./functions/boostImage'),
-    boost = require('./functions/boost')
+	passGen = require('./functions/passGen'),
+	bin = require('./functions/bin'),
+	inviteLogger = require('./functions/inviteLogger'),
+	_checkUpdate = require('./functions/checkUpdates'),
+	buttonPaginator = require('./functions/buttonPaginator'),
+	welcomeImage = require('./functions/welcomeImage'),
+	Wikipedia = require('./classes/wikipedia'),
+	sussybaka = require('./functions/sussybaka'),
+	remind = require('./functions/remind'),
+	logger = require('./functions/logger'),
+	dababy = require('./functions/dababy'),
+	boostImage = require('./functions/boostImage'),
+	boost = require('./functions/boost');
 
-_checkUpdate()
+_checkUpdate();
 module.exports = {
-    sleep,
-    passGen,
-    bin,
-    inviteLogger,
-    ButtonPaginator,
-    welcomeImage,
-    Wikipedia,
-    sussyBaka,
-    connectToMongoDB,
-    remind,
-    logger,
-    daBaby,
-    boostImage,
-    boost
+	sleep,
+	passGen,
+	bin,
+	inviteLogger,
+	buttonPaginator,
+	welcomeImage,
+	Wikipedia,
+	sussybaka,
+	connectToMongoDB,
+	remind,
+	logger,
+	dababy,
+	boostImage,
+	boost,
 };
 
 /**
@@ -38,14 +38,14 @@ module.exports = {
  * @returns {void}
  */
 function connectToMongoDB(MongoDBURI) {
-    let connected = true;
-    db.connect(MongoDBURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).catch(e => {
-        connected = false;
-        throw e;
-    }).then(() => {
-        if (connected === true) console.info("[UltraX] => Connected to DB successfully.")
-    });
+	let connected = true;
+	db.connect(MongoDBURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}).catch(e => {
+		connected = false;
+		throw e;
+	}).then(() => {
+		if (connected === true) console.info('[UltraX] => Connected to DB successfully.');
+	});
 }
