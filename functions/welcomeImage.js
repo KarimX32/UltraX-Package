3bb307c633c7259886b167a7a305ad6436fe75f4
HyperNaili <gamernaili.101010@gmail.com>
2022-08-25 21:15:09 +0200

    refacor: ErrorManager, destructing, name changes and more
    This commit includes:
    - Added ErrorManager for better errors
    - Changed to destructing for discord.js
    - Changed the JSDoc
    - Changed options to more logical names:
      - text_1 : title
      - text_2 : subtitle
      - text_3 : footer
      - text1_fontSize : title_fontSize
      - text2_fontSize : subtitle_fontSize
      - text3_fontSize : footer_fontSize
    - Removed the checks for canvas because it gets auto installed with the installation of the package.

--- functions/welcomeImage.js
+++ functions/welcomeImage.js
@@ -1,87 +1,89 @@
-const Discord = require("discord.js");
+const { MessageAttachment } = require('discord.js');
+const { Error, TypeError } = require('../classes/non-public-classes/Error');
+const Canvas = require('canvas');
 
 /**
  * Create a welcome image for Discord bot using discord.js
  * @param {String} background The image background
- * @param {String} avatar The avatar
- * @param {String} text_1 The title
- * @param {String} text_2 The subtitle
- * @param {String} text_3 The footer
- * @param {String} color The color (hex code)
- * @param {Object | String} settings The attachment
- * @returns {Promise<Discord.MessageAttachment>}
+ * @param {String} avatar The avatar for the image
+ * @param {String} title The title for the image
+ * @param {String} subtitle The subtitle for the image
+ * @param {String} footer The footer for the image
+ * @param {String} color The color for the image
+ * @param {import("../index").WelcomeImageSettingsOptions} [settings] The settings for the image
+ * @returns {Promise<MessageAttachment>}
  */
-module.exports = async function welcomeImage(background, avatar, text_1, text_2, text_3, color, settings) {
-    try {
-        require.resolve("canvas")
-    } catch (e) {
-        throw new Error("[UltraX] => Cannot find module 'canvas' Please do ' npm i canvas@latest '")
-    }
-    const Canvas = require("canvas");
-    if (!background) throw new SyntaxError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> background is null.")
-    if (!background.endsWith('.png')) throw new Error("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> background must be a PNG.")
-    if (!avatar) throw new SyntaxError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> avatar is null")
-    if (!text_1) throw new SyntaxError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> text_1 is null")
-    if (!text_2) throw new SyntaxError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> text_2 is null")
-    if (!text_3) throw new SyntaxError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> text_3 is null")
-    if (!color) throw new SyntaxError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> color is null")
-    if (!/^#[0-9A-F]{6}$/i.test(color) == true && !/^#([0-9A-F]{3}){1,2}$/i.test(color) == true) throw new ReferenceError("welcomeImage(background, avatar, text_1, text_2, text_3, color) ==> color is not a valid hex")
-    let fonts;
-    let attachmentName;
-    let text1_fontSize;
-    let text2_fontSize;
-    let text3_fontSize;
+module.exports = async function welcomeImage(background, avatar, title, subtitle, footer, color, settings) {
+	if (!background) throw new Error('MISSING_PARAMETER', "The parameter 'background' is missing");
+	if (!background.endsWith('.png')) throw new Error('PARAMETER_NOT_PNG', "The parameter 'background' is not a PNG");
+	if (!avatar) throw new Error('MISSING_PARAMETER', "The parameter 'avatar' is missing");
+	if (!title) throw new Error('MISSING_PARAMETER', "The parameter 'title' is missing");
+	if (!subtitle) throw new Error('MISSING_PARAMETER', "The parameter 'subtitle' is missing");
+	if (!footer) throw new Error('MISSING_PARAMETER', "The parameter 'footer' is missing");
+	if (!color) throw new Error('MISSING_PARAMETER', "The parameter 'color' is missing");
+	if (typeof background !== 'string') throw new TypeError('PARAMETER_NOT_STRING', "The parameter 'background' must be a string");
+	if (typeof avatar !== 'string') throw new TypeError('PARAMETER_NOT_STRING', "The parameter 'avatar' must be a string");
+	if (typeof title !== 'string') throw new TypeError('PARAMETER_NOT_STRING', "The parameter 'title' must be a string");
+	if (typeof subtitle !== 'string') throw new TypeError('PARAMETER_NOT_STRING', "The parameter 'subtitle' must be a string");
+	if (typeof footer !== 'string') throw new TypeError('PARAMETER_NOT_STRING', "The parameter 'footer' must be a string");
+	if (typeof color !== 'string') throw new TypeError('PARAMETER_NOT_STRING', "The parameter 'color' must be a string");
+	if (!/^#[0-9A-F]{6}$/i.test(color) == true && !/^#([0-9A-F]{3}){1,2}$/i.test(color) == true) throw new Error('PARAMETER_NOT_HEX', "The parameter 'color' is not a hexadecimal color");
+	let font;
+	let attachmentName;
+	let title_fontSize;
+	let subtitle_fontSize;
+	let footer_fontSize;
 
-    if (!settings || !settings.font) fonts = "sans-serif"
-    else fonts = settings.font
+	if (!settings || !settings.font) font = 'sans-serif';
+	else font = settings.font;
 
-    if (!settings || !settings.attachmentName) attachmentName = "welcome"
-    else attachmentName = settings.attachmentName
+	if (!settings || !settings.attachmentName) attachmentName = 'welcome';
+	else attachmentName = settings.attachmentName;
 
-    if (!settings || !settings.text1_fontSize) text1_fontSize = 72
-    else if (isNaN(settings.text1_fontSize)) throw new Error("text1_fontSize ==> The text 1 font size isNaN")
-    else if (settings.text1_fontSize < 5) throw new Error("text1_fontSize ==> The text 1 font size cannot be less than 5px")
-    else text1_fontSize = settings.text1_fontSize
+	if (!settings || !settings.title_fontSize) title_fontSize = 72;
+	else if (isNaN(settings.title_fontSize)) throw new Error('PARAMETER_NAN', "The parameter 'settings.title_fontSize' is not a number");
+	else if (settings.title_fontSize < 5) throw new Error('PARAMETER_LESS_THAN_5', "The parameter 'settings.title_fontSize' is less than 5px");
+	else title_fontSize = settings.title_fontSize;
 
-    if (!settings || !settings.text2_fontSize) text2_fontSize = 42
-    else if (isNaN(settings.text2_fontSize)) throw new Error("text2_fontSize ==> The text 2 font size isNaN")
-    else if (settings.text2_fontSize < 5) throw new Error("text2_fontSize ==> The text 2 font size cannot be less than 5px")
-    else text2_fontSize = settings.text2_fontSize
+	if (!settings || !settings.subtitle_fontSize) subtitle_fontSize = 42;
+	else if (isNaN(settings.subtitle_fontSize)) throw new Error('PARAMETER_NAN', "The parameter 'settings.subtitle_fontSize' is not a number");
+	else if (settings.subtitle_fontSize < 5) throw new Error('PARAMETER_LESS_THAN_5', "The parameter 'settings.subtitle_fontSize' is less than 5px");
+	else subtitle_fontSize = settings.subtitle_fontSize;
 
-    if (!settings || !settings.text3_fontSize) text3_fontSize = 32
-    else if (isNaN(settings.text3_fontSize)) throw new Error("text3_fontSize ==> The text 3 font size isNaN")
-    else if (settings.text3_fontSize < 5) throw new Error("text3_fontSize ==> The text 3 font size cannot be less than 5px")
-    else text3_fontSize = settings.text3_fontSize
+	if (!settings || !settings.footer_fontSize) footer_fontSize = 32;
+	else if (isNaN(settings.footer_fontSize)) throw new Error('PARAMETER_NAN', "The parameter 'settings.footer_fontSize' is not a number");
+	else if (settings.footer_fontSize < 5) throw new Error('PARAMETER_LESS_THAN_5', "The parameter 'settings.footer_fontSize' is less than 5px");
+	else footer_fontSize = settings.footer_fontSize;
 
 
-    var welcomeCanvas = {};
-    welcomeCanvas.create = Canvas.createCanvas(1024, 500)
-    welcomeCanvas.context = welcomeCanvas.create.getContext('2d')
-    const ctx = welcomeCanvas.context
-    ctx.font = `${text1_fontSize}px ${fonts}`;
-    ctx.fillStyle = color;
+	const welcomeCanvas = {};
+	welcomeCanvas.create = Canvas.createCanvas(1024, 500);
+	welcomeCanvas.context = welcomeCanvas.create.getContext('2d');
+	const ctx = welcomeCanvas.context;
+	ctx.font = `${title_fontSize}px ${font}`;
+	ctx.fillStyle = color;
 
-    const bg = await Canvas.loadImage(background)
-    ctx.drawImage(bg, 0, 0, 1024, 500)
-    ctx.textAlign = 'center'
-    ctx.fillText(text_1, 512, 360);
-    ctx.beginPath();
-    ctx.arc(512, 166, 128, 0, Math.PI * 2, true);
-    ctx.stroke()
-    ctx.fill()
-    let canvas = welcomeCanvas;
-    canvas.context.font = `${text2_fontSize}px ${fonts}`,
-        canvas.context.textAlign = 'center';
-    canvas.context.fillText(text_2, 512, 410)
-    canvas.context.font = `${text3_fontSize}px ${fonts}`
-    canvas.context.fillText(text_3, 512, 455)
-    canvas.context.beginPath()
-    canvas.context.arc(512, 166, 119, 0, Math.PI * 2, true)
-    canvas.context.closePath()
-    canvas.context.clip()
-    await Canvas.loadImage(avatar)
-        .then(img => {
-            canvas.context.drawImage(img, 393, 47, 238, 238);
-        })
-        return new Discord.MessageAttachment(canvas.create.toBuffer(), `${attachmentName}.png`);
-}
+	const bg = await Canvas.loadImage(background);
+	ctx.drawImage(bg, 0, 0, 1024, 500);
+	ctx.textAlign = 'center';
+	ctx.fillText(title, 512, 360);
+	ctx.beginPath();
+	ctx.arc(512, 166, 128, 0, Math.PI * 2, true);
+	ctx.stroke();
+	ctx.fill();
+	const canvas = welcomeCanvas;
+	canvas.context.font = `${subtitle_fontSize}px ${font}`,
+	canvas.context.textAlign = 'center';
+	canvas.context.fillText(subtitle, 512, 410);
+	canvas.context.font = `${footer_fontSize}px ${font}`;
+	canvas.context.fillText(footer, 512, 455);
+	canvas.context.beginPath();
+	canvas.context.arc(512, 166, 119, 0, Math.PI * 2, true);
+	canvas.context.closePath();
+	canvas.context.clip();
+	await Canvas.loadImage(avatar)
+		.then(img => {
+			canvas.context.drawImage(img, 393, 47, 238, 238);
+		});
+	return new MessageAttachment(canvas.create.toBuffer(), `${attachmentName}.png`);
+};
