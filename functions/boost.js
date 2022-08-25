65c686f4bc85fc551ee6af9684875c42e22677e8
HyperNaili <gamernaili.101010@gmail.com>
2022-08-25 15:34:29 +0200

    refactor: destructing for discord.js, added ErrorManager and more
    This commit includes:
    - Fixed the problem where boost notifications wouldn't stack if more than one boost would be boosted by using GuildMember.premiumSinceTimestamp.
    - Removed boostRoleID parameter since it's no longer needed.
    - Updated JSDoc.
    - Changed Discord variable to destructing.

--- functions/boost.js
+++ functions/boost.js
@@ -1,22 +1,17 @@
-const Discord = require("discord.js")
-
+const { Client } = require('discord.js'); // eslint-disable-line no-unused-vars
+const { Error } = require('../classes/non-public-classes/Error');
 
 /**
- * Starts a boost event
- * @param {Discord.Client} client Your client
- * @param {Discord.Snowflake} boostRoleID The ID of the booster role in the server
- * @returns {Promise<void>}
+ * Initialize the event listener into the client
+ * @param {Client} client Your client
+ * @returns {void}
  */
-module.exports.start = async (client, boostRoleID) => {
-    if (!client) throw new SyntaxError("Missing the parameter 'client'");
-    if (!boostRoleID) throw new SyntaxError("Missing the parameter 'boostRoleID'");
+module.exports.init = (client) => {
+	if (!client) throw new Error('MISSING_PARAMETER', "The parameter 'client' is missing");
 
-    client.on('guildMemberUpdate', async (oldMember, newMember) => {
-        if (!newMember.guild.roles.cache.get(boostRoleID)) throw new Error("Given ID is invalid or has no matching role in the guild");
-
-        if (!oldMember.roles.cache.some(role => role.id === boostRoleID) && newMember.roles.cache.some(role => role.id === boostRoleID)) {
-            client.emit('boost', newMember)
-        };
-    });
-
-}
+	client.on('guildMemberUpdate', (oldMember, newMember) => {
+		if (oldMember.premiumSinceTimestamp != newMember.premiumSinceTimestamp) {
+			client.emit('boost', newMember);
+		}
+	});
+};
